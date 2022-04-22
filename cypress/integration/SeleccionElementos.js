129902533aa97befe7c4aa5995bab6ec56d55c46
Fco. Javier Iranzo Rebenaque <javieriranzo92@gmail.com>
2022-04-22 14:34:34 +0200

    Seleccion elementos por class y por selector

--- cypress/integration/SeleccionElementos.js
+++ cypress/integration/SeleccionElementos.js
@@ -12,4 +12,16 @@ describe("Test - Seleccion elementos", function() {
         cy.get("[name='q']");
     });
 
+    it("Seleccionar elemento por class", function() {
+        cy.visit("https://www.google.es/");
+        cy.get("[name='q']").type("Francisco Javier Iranzo Rebenaque {enter}");
+        cy.get(".LC20lb");
+    });
+
+    it("Seleccionar elemento por selector", function() {
+        cy.visit("https://www.google.es/");
+        cy.get("[name='q']").type("Francisco Javier Iranzo Rebenaque {enter}");
+        cy.get("#rso > div:nth-child(1) > div > div:nth-child(1) > div > div > div.NJo7tc.Z26q7c.jGGQ5e > div > a > h3").click();
+    })
+
 })
