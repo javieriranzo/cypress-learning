ebf524829ba25b0416f20bdba2498696b13fbe47
Fco. Javier Iranzo Rebenaque <javieriranzo92@gmail.com>
2022-04-22 14:35:10 +0200

    Funcion typeEnter para accionar el enter despues de escribir en un input

--- cypress/integration/FuncionTypeEnter.js
+++ cypress/integration/FuncionTypeEnter.js
@@ -3,7 +3,8 @@ describe("Test - Funcion TypeEnter", function() {
 
     it("Funcion para cuando se presione 'Enter'", function() {
         cy.visit("https://www.google.es/");
-        cy.get("[name='q']").type("Francisco Javier Iranzo Rebenaque");
+        cy.get("[name='q']").type("Francisco Javier Iranzo Rebenaque {enter}");
+        cy.get("#rso > div:nth-child(1) > div > div:nth-child(1) > div > div > div.NJo7tc.Z26q7c.jGGQ5e > div > a > h3").click();
     });
 
-})
+});
