5e22081b90fb01fad181d23a9ce365c64f57601a
fjiranzo.ITI <jiranzo@iti.es>
2022-09-27 16:59:21 +0200

    Add comments

--- cypress/e2e/typeEnterFuncition.cy.js
+++ cypress/e2e/typeEnterFuncition.cy.js
@@ -6,10 +6,9 @@ describe("Type enter function", function() {
         cy.visit("www.google.es")
         cy.title().should('eq', 'Google')
         cy.wait(4000) // Sleep execution for 1500ms
-        cy.get("#L2AGLb").click()
+        cy.get("#L2AGLb").click() // click button to close dialog
         cy.wait(2000)
-        cy.get("[name='q']").type("Cypress io {enter}") 
-        // {enter} for press intro key
+        cy.get("[name='q']").type("Cypress io {enter}") // {enter} for press intro key   
     });
     
 });
