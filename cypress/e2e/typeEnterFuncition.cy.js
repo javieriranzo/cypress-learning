e59c00090e6aab70b7d181cee37d7da7453ac797
fjiranzo.ITI <jiranzo@iti.es>
2022-09-27 16:58:02 +0200

    Error with tpyeEnter funciton fixed. Its covered by another element

--- cypress/e2e/typeEnterFuncition.cy.js
+++ cypress/e2e/typeEnterFuncition.cy.js
@@ -5,7 +5,9 @@ describe("Type enter function", function() {
     it("Test for type enter function", function() {
         cy.visit("www.google.es")
         cy.title().should('eq', 'Google')
-        cy.wait(1500) // Sleep execution for 1500ms
+        cy.wait(4000) // Sleep execution for 1500ms
+        cy.get("#L2AGLb").click()
+        cy.wait(2000)
         cy.get("[name='q']").type("Cypress io {enter}") 
         // {enter} for press intro key
     });
