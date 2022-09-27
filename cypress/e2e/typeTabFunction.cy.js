2fa4b6f61af2a0431c72b599c92a000c00ceba04
fjiranzo.ITI <jiranzo@iti.es>
2022-09-27 18:18:19 +0200

    Added tab library for tab functions

---
+++ cypress/e2e/typeTabFunction.cy.js
@@ -0,0 +1,10 @@
+/// <reference types="Cypress" />
+
+describe("Type tab", function() {
+    
+    it("Test for type tab function", function() {
+        cy.visit("https://testingqarvn.com.es/datos-personales/")
+        cy.title().should('eq', 'Datos Personales | TestingQaRvn') 
+    })
+    
+});
