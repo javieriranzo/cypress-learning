1052111b72ab49594bcac56250c627ff699606c7
fjiranzo.ITI <jiranzo@iti.es>
2022-09-27 18:24:57 +0200

    Tab function example

--- cypress/e2e/typeTabFunction.cy.js
+++ cypress/e2e/typeTabFunction.cy.js
@@ -7,6 +7,12 @@ describe("Type tab", function() {
     it("Test for type tab function", function() {
         cy.visit("https://testingqarvn.com.es/datos-personales/")
         cy.title().should('eq', 'Datos Personales | TestingQaRvn') 
+        cy.get("#wsf-1-field-21").type("Francisco Javier")
+        .tab().type("Iranzo Rebenaque")
+        .tab().type("javieriranzo92@gmail.com")
+        .tab().type("666305652")
+        .tab().type("Albalat dels Sorells, Valencia")
+        cy.wait(10000)
     })
     
 });
