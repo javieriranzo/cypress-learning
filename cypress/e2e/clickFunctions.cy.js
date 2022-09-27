eec29bb37e557148aace0fa5698eb3ea7597c13b
fjiranzo.ITI <jiranzo@iti.es>
2022-09-28 00:32:54 +0200

    Click functions

---
+++ cypress/e2e/clickFunctions.cy.js
@@ -0,0 +1,35 @@
+/// <reference types="Cypress" />
+
+describe("Click functions", function() {
+
+    it("Sample click - Return error message because we dont type the user and password", function() {
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        cy.title().should('eq', 'OrangeHRM')
+        cy.get('.oxd-button').should('be.visible').click()
+    })
+
+    it("Click button - Enter username, password and then click login button", function() {
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        cy.title().should('eq', 'OrangeHRM')
+        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+        cy.get('.oxd-button').should('be.visible').click()
+    })
+
+    it("Validate fields - Enter username, password and then press enter", function() {
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        cy.title().should('eq', 'OrangeHRM')
+        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type("admin123 {enter}")
+        cy.wait(5000)
+    })
+
+    it("Force click - Click force true", function() {
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        cy.title().should('eq', 'OrangeHRM')
+        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+        cy.get('.oxd-button').should('be.visible').click({force:true})
+    })
+
+})
