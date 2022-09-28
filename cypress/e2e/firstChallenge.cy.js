b083e070bd81f7cbcf14d745c453ecf0b0b3e42e
fjiranzo.ITI <jiranzo@iti.es>
2022-09-28 17:13:29 +0200

    First challenge completed

--- cypress/e2e/firstChallenge.cy.js
+++ cypress/e2e/firstChallenge.cy.js
@@ -2,10 +2,33 @@
 
 describe("First challenge", function() {
 
-    it("Challenge step 1: Enter and validate texts", function() {
+    it("Challenge step 1 - Login", function() {
         cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
         cy.title().should('eq', 'OrangeHRM')
-        cy.get('.oxd-text--h5').should('eq', 'Login')
+        cy.get("[name='username']").type('Admin')
+        cy.get("[name='password']").type('admin123')
         cy.get('.oxd-button').should('be.visible').click()
+        cy.get('#app > div.oxd-layout > div.oxd-layout-navigation > header > div.oxd-topbar-header > div.oxd-topbar-header-title > span > h6').should('be.visible')
+    })
+
+    it("Challenge step 2 - Visit 'Admin Page'", function() {
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        cy.title().should('eq', 'OrangeHRM')
+        cy.get("[name='username']").type('Admin')
+        cy.get("[name='password']").type('admin123')
+        cy.get('.oxd-button').should('be.visible').click()
+        cy.get('#app > div.oxd-layout > div.oxd-layout-navigation > aside > nav > div.oxd-sidepanel-body > ul > li:nth-child(1) > a').should('be.visible').click()
+        cy.get('#app > div.oxd-layout > div.oxd-layout-navigation > header > div.oxd-topbar-body > nav > ul > li.oxd-topbar-body-nav-tab.--parent.--visited > span').should('be.visible')
+    })
+
+    it("Challegne step 3 - Search a registred user", function() {
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        cy.title().should('eq', 'OrangeHRM')
+        cy.get("[name='username']").type('Admin')
+        cy.get("[name='password']").type('admin123')
+        cy.get('.oxd-button').should('be.visible').click()
+        cy.get('#app > div.oxd-layout > div.oxd-layout-navigation > aside > nav > div.oxd-sidepanel-body > ul > li:nth-child(1) > a').should('be.visible').click()
+        cy.get('#app > div.oxd-layout > div.oxd-layout-navigation > header > div.oxd-topbar-body > nav > ul > li.oxd-topbar-body-nav-tab.--parent.--visited > span').should('be.visible')
+        cy.get('#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div.oxd-table-filter > div.oxd-table-filter-area > form > div.oxd-form-row > div > div:nth-child(1) > div > div:nth-child(2) > input').should('be.visible').type('Aaliyah.Haq')
     })
 });
