bb150acc82c6482033d8f4430ae3cde3f6883b63
fjiranzo.ITI <jiranzo@iti.es>
2022-10-11 19:48:46 +0200

    Validate product price with asserts

--- cypress/e2e/asserts/assertText.cy.js
+++ cypress/e2e/asserts/assertText.cy.js
@@ -1,9 +1,8 @@
 /// <reference types="Cypress" />
 
-
 describe("Asserts Text", function() {
    
-    it("Assert Text - ", function() {
+    it("Assert Text - Validate product condition", function() {
         cy.visit("http://automationpractice.com/index.php")
         cy.title().should('eq', 'My Store')
         cy.get("#block_top_menu").contains('Women').click()
@@ -14,6 +13,24 @@ describe("Asserts Text", function() {
             if (productCondition == "New") {
                 cy.log("New product")
             }
+        })
+    }),
+
+    it("Assert Text - Validate product price", function () {
+        cy.visit("http://automationpractice.com/index.php")
+        cy.title().should('eq', 'My Store')
+        cy.get("#block_top_menu").contains('Women').click()
+        cy.get(".product-container").find(".product-image-container").eq(2).click()
+        
+        cy.get("#our_price_display").then((e) => {
+            let productPrice = e.text()
+            // Delete dollar symbol
+            productPrice = productPrice.slice(1,6)
+            /*
+            productPrice = parseInt(productPrice)
+            cy.log(productPrice)
+            cy.log(isNaN(productPrice))
+            */
         })
     })
     
