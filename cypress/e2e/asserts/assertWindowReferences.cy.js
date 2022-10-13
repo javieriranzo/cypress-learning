717cc14faa4abdf32677bb9c2666e616d61ee811
fjiranzo.ITI <jiranzo@iti.es>
2022-10-13 23:24:38 +0200

    Format code

--- cypress/e2e/asserts/assertWindowReferences.cy.js
+++ cypress/e2e/asserts/assertWindowReferences.cy.js
@@ -4,12 +4,9 @@ describe("Asserts Challenge", function() {
    
     it("Assert challenge - Window references - Charset property", function() {
         // Test web charset property
-        let charsetProperty = "utf-8"
         cy.visit("https://testsheepnz.github.io/random-number.html")
         cy.title().should('eq', 'The Number Game')
         cy.document().should("have.property", "charset").and("eq", "UTF-8")
     })
-
     
-
 })
