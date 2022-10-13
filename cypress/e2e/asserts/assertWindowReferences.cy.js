3063944734a7fda898cbeee9521f4571937efce6
fjiranzo.ITI <jiranzo@iti.es>
2022-10-13 23:29:43 +0200

    Validate URLs

--- cypress/e2e/asserts/assertWindowReferences.cy.js
+++ cypress/e2e/asserts/assertWindowReferences.cy.js
@@ -8,5 +8,15 @@ describe("Asserts Challenge", function() {
         cy.title().should('eq', 'The Number Game')
         cy.document().should("have.property", "charset").and("eq", "UTF-8")
     })
-    
+
+    it("Assert challenge - Window references - Validate URL", function() {
+        // Validate URL
+        cy.visit("https://testsheepnz.github.io/random-number.html")
+        cy.title().should('eq', 'The Number Game')
+        // Validate only a slice of URL 
+        cy.url().should("include", "/random-number.html")
+        // Validate complete URL
+        cy.url().should("eq", "https://testsheepnz.github.io/random-number.html")
+    })
+
 })
